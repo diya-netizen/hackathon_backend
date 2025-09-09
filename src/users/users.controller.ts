d0a925d63c62aa45c5b501a205c333a0d660e9cc
Diya Ranjit <diya@epiqscripts.com>
2025-09-08 22:55:01 -0500

    backend code

---
+++ src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Post, Body, Get, Query, Patch, Param, Delete, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './users.entity';
+import type { Request } from 'express';
+
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    createUser(@Body() data: any) {
+        return this.usersService.create(data);
+    }
+
+    @Get()
+    async getUsers(@Query() query, @Req() req: Request,) {
+        if(!req.session.userId){
+            return { message: 'User not authenticated.', success: false};
+        }
+
+        const page = parseInt(query.page, 10) || 1;
+        const limit = parseInt(query.limit, 10) || 10;
+
+        const users = await this.usersService.getUsers(page, limit);
+
+        return {
+            success: true,
+            ...users,
+        };
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: number) {
+        return this.usersService.delete(id);
+    }
+    
+    @Patch(':id')
+    async updateUser(
+    @Param('id') id: number,
+    @Body() data: Partial<User>,
+    ) {
+        return this.usersService.update(id, data);
+    }
+}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,29 @@
+// src/users/user.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: 'Active' })
+  status: string;
+
+  @Column({ default: 'User' })
+  role: string;
+}
